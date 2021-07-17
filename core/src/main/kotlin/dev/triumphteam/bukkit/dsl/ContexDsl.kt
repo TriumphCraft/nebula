d40886ef8c51b211b789efa3f7c43ef8c6bb2d51
Matt <xpsykogfx@gmail.com>
2021-07-17 16:31:52 +0100

    Added missing licenses

--- core/src/main/kotlin/dev/triumphteam/bukkit/dsl/ContexDsl.kt
+++
@@ -1,5 +0,0 @@
-package dev.triumphteam.bukkit.dsl
-
-@DslMarker
-@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPEALIAS, AnnotationTarget.TYPE, AnnotationTarget.FUNCTION)
-public annotation class TriumphDsl
